9d89cb737e657ac3aeec0d6d697d605021dd176e
Ranjith J Pillai <ranjithj.pillai@philips.com>
2024-08-27 18:11:39 +0530

    Finished assignment

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,18 @@
 #include "stats.h"
+#include <algorithm>
+#include <numeric>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+using namespace Statistics;
+
+Stats Statistics::ComputeStatistics(const std::vector<double>& stats) {
+
+    struct Stats output;
+
+    if (!stats.empty()) {
+        output.average = ((std::accumulate(stats.begin(), stats.end(), 0.0)) / stats.size());
+        output.max = std::max_element(stats.begin(), stats.end()).operator*();
+        output.min = std::min_element(stats.begin(), stats.end()).operator*();
+    }
+    return output;
 }
